36d53cb9a8f243597640f8d3a6e9ef36f7468a53
KIN <KIN@DESKTOP-URHOUHL>
2019-07-31 21:15:47 +0900

    aa

--- MGM/src/main/java/com/gam/mgm/AdminController.java
+++ MGM/src/main/java/com/gam/mgm/AdminController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,8 @@ import com.gam.mgm.service.ITrainerService;
 @Controller
 public class AdminController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
-	ITrainerService trainerService;
+	@Autowired
+	private ITrainerService trainerService;
 	@RequestMapping(value="/adminPage.do",method=RequestMethod.GET)
 	public String adminPage() {
 		return "Admin/AdTemp";
